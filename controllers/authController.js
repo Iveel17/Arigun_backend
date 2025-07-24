c1cc8f060369e7e449ffe4f087f2b2b709ea4d68
Munkh-Iveel <iveelv968@gmail.com>
2025-07-24 18:51:23 +0800

    added USer.js as well as models folder but currently only takes email and password might add names soon

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+import User from '../models/User.js';
+
 // controllers/authController.js
 
 export const signup_get = (req, res) => {
@@ -13,13 +15,20 @@ export const login_get = (req, res) => {
 export const signup_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send('new signup');
-};
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch(err) {
+    console.log(err);
+    res.status(400).send('error, user not created');
+  }
+
+}
 
 export const login_post = async (req, res) => {
   const { email, password } = req.body;
 
   console.log(email, password);
   res.send('user login');
-};
+}
