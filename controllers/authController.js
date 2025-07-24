5dcb699354945b44f2954a289253734fa277adff
Munkh-Iveel <iveelv968@gmail.com>
2025-07-24 18:26:21 +0800

    added bit tricky thing to controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,10 +8,18 @@ export const login_get = (req, res) => {
   res.render('login');
 };
 
+// controllers/authController.js
+
 export const signup_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send('new signup');
 };
 
 export const login_post = async (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email, password);
   res.send('user login');
 };
