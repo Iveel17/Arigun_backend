de313fd275c84be6b061f1d5a33865f3c05c8e55
Munkh-Iveel <iveelv968@gmail.com>
2025-07-27 11:35:02 +0800

    added validator and now knows if user is already there or not

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,26 @@
 import User from '../models/User.js';
 
-// controllers/authController.js
+const handleErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
 
+  // duplicate email error
+  if (err.code === 11000) {
+    errors.email = 'that email is already registered';
+    return errors;
+  }
+
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    Object.values(err.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+};
+
+// controller actions
 export const signup_get = (req, res) => {
   res.render('signup');
 };
@@ -10,25 +29,21 @@ export const login_get = (req, res) => {
   res.render('login');
 };
 
-// controllers/authController.js
-
 export const signup_post = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.create({ email, password });
     res.status(201).json(user);
+  } catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
-  catch(err) {
-    console.log(err);
-    res.status(400).send('error, user not created');
-  }
-
-}
+};
 
 export const login_post = async (req, res) => {
   const { email, password } = req.body;
 
   console.log(email, password);
   res.send('user login');
-}
+};
--- models/User.js
+++ models/User.js
@@ -1,20 +1,19 @@
 import mongoose from 'mongoose';
+import validator from 'validator';
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please enter an email'],
     unique: true,
     lowercase: true,
-    trim: true,
+    validate: [validator.isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 6,
+    required: [true, 'Please enter a password'],
+    minlength: [6, 'Minimum password length is 6 characters']
   }
-}, {
-  timestamps: true,
 });
 
 const User = mongoose.model('User', userSchema);
