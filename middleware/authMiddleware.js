0473873f17de8b50ba77440172d69aeaf48c0565
Munkh-Iveel <iveelv968@gmail.com>
2025-08-19 18:39:43 +0800

    ez

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -13,7 +13,6 @@ const requireAuth = (allowGuest = false) => {
       if (allowGuest) {
         // Set guest user
         req.user = User.getGuestUser();
-        res.locals.user = req.user;
         return next();
       }
       return res.status(401).json({ 
@@ -30,7 +29,6 @@ const requireAuth = (allowGuest = false) => {
       if (!user) {
         if (allowGuest) {
           req.user = User.getGuestUser();
-          res.locals.user = req.user;
           return next();
         }
         return res.status(401).json({ 
@@ -40,14 +38,12 @@ const requireAuth = (allowGuest = false) => {
       }
 
       req.user = user;
-      res.locals.user = user;
       next();
     } catch (err) {
       console.error('JWT verification failed:', err.message);
       
       if (allowGuest) {
         req.user = User.getGuestUser();
-        res.locals.user = req.user;
         return next();
       }
       
@@ -78,34 +74,6 @@ const requireRole = (...roles) => {
         message: 'Insufficient permissions',
         required: roles,
         current: req.user.role
-      });
-    }
-
-    next();
-  };
-};
-
-// Permission-based authorization middleware
-const requirePermission = (...permissions) => {
-  return (req, res, next) => {
-    if (!req.user) {
-      return res.status(401).json({ 
-        success: false, 
-        message: 'Authentication required' 
-      });
-    }
-
-    // Check if user has any of the required permissions
-    const hasRequiredPermission = permissions.some(permission => 
-      req.user.hasPermission(permission)
-    );
-    
-    if (!hasRequiredPermission) {
-      return res.status(403).json({ 
-        success: false, 
-        message: 'Insufficient permissions',
-        required: permissions,
-        userRole: req.user.role
       });
     }
 
@@ -148,36 +116,6 @@ const requireOwnershipOrAdmin = (getResourceUserId) => {
   };
 };
 
-// Enhanced checkUser that includes role information
-const checkUser = (req, res, next) => {
-  const token = req.cookies.jwt;
-  
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
-      if (err) {
-        req.user = User.getGuestUser();
-        res.locals.user = req.user;
-        next();
-      } else {
-        try {
-          const user = await User.findById(decodedToken.id).select('-password');
-          req.user = user || User.getGuestUser();
-          res.locals.user = req.user;
-          next();
-        } catch (error) {
-          req.user = User.getGuestUser();
-          res.locals.user = req.user;
-          next();
-        }
-      }
-    });
-  } else {
-    req.user = User.getGuestUser();
-    res.locals.user = req.user;
-    next();
-  }
-};
-
 // Utility function to create route protection combinations
 const createProtectedRoute = (roles = [], permissions = [], allowGuest = false) => {
   const middlewares = [requireAuth(allowGuest)];
@@ -186,9 +124,6 @@ const createProtectedRoute = (roles = [], permissions = [], allowGuest = false) 
     middlewares.push(requireRole(...roles));
   }
   
-  if (permissions.length > 0) {
-    middlewares.push(requirePermission(...permissions));
-  }
   
   return middlewares;
 };
@@ -196,8 +131,6 @@ const createProtectedRoute = (roles = [], permissions = [], allowGuest = false) 
 export { 
   requireAuth, 
   requireRole, 
-  requirePermission, 
   requireOwnershipOrAdmin,
-  checkUser,
   createProtectedRoute
 };
